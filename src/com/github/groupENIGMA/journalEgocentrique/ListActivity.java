5f206cf90b14083b214db17bab7a5edce94677fb
matteo <matteovalerio2006@hotmail.it>
2013-05-20 16:34:19 +0200

    Cleaning the code.

--- src/com/github/groupENIGMA/journalEgocentrique/ListActivity.java
+++ src/com/github/groupENIGMA/journalEgocentrique/ListActivity.java
@@ -4,13 +4,11 @@ import java.util.Calendar;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -18,7 +16,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.github.groupENIGMA.journalEgocentrique.model.DB;
 import com.github.groupENIGMA.journalEgocentrique.model.Entry;
@@ -33,16 +30,46 @@ public class ListActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
-	    /*
-	     * Popolazione della ListView con i dati forniti dal database
-	     * Viene inoltre creato un listener sulla lista che al click
-	     * di un certo giorno, visualizza i dettagli dello stesso
-	     */
+
+		setContentView(R.layout.main);
+	    dataBase = new DB();
 	    menu = dataBase.getDays();
-	    ListView entry = (ListView)findViewById(R.id.listView1);
+
+	    ListView list = (ListView)findViewById(R.id.list);
+	    
+	    setListView(list, menu);
+	    
+	    setImages(selectedEntry);
+	    
+	    setNotes(selectedEntry);
+	    }
+	
+/**
+ * Displays the correct notes for the entry selected by the user.
+ * @param selected Entry. The entry selected by the user.
+ */
+	private void setNotes(Entry selected) {
+    	boolean editable = selectedEntry.canBeUpdated();
+	    LinearLayout notes = (LinearLayout)findViewById(R.id.notes);
+	    List<Note> myNotes = selectedEntry.getNotes();
+	    for(int i = 0;i < myNotes.size();i++){
+	    	EditText tmp = new EditText(this);
+	    	tmp.append(myNotes.get(i).toString());
+	    	tmp.setFocusable(editable);
+	    	notes.addView(tmp);
+	    }
+	}
+
+
+	/**
+	 * The ListView will be populated with the data given by the database.
+	 * It is also created a OnItemClickListener that at the click will display the details of the day.
+	 * @param list ListView. The list to populate.
+	 * @param entry List<Calendar> With this List we will populate the ListView 
+	 */
+	private void setListView(ListView list, List<Calendar> entry){
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.row, R.id.textViewList);
-        entry.setAdapter(arrayAdapter);
+        list.setAdapter(arrayAdapter);
         OnItemClickListener clickListener = new OnItemClickListener() {
 
             @Override
@@ -51,13 +78,19 @@ public class ListActivity extends Activity {
                 selectedEntry = (Entry)adapter.getItemAtPosition(position);
             }
         };
-        entry.setOnItemClickListener(clickListener);
-        /*
-         * Aggiorna i dati sulla schermata d'inizio
-         * manca notes
-         */
-		setContentView(R.layout.main);
-	       if(selectedEntry == null){
+        list.setOnItemClickListener(clickListener);
+	}
+	
+	
+	/**
+	 * Sets the correct image for photo and mood selected by the user.
+	 * @param selected Entry. The entry selected by the user, its details will be displayed.
+	 */
+	private void setImages(Entry selected){
+	   /*
+	    * Caso iniziale:nessun entry selezionata 
+	    */
+		if(selected == null){
     	    ImageView img = (ImageView) findViewById(R.id.dailyPhoto); 
     	    img.setImageResource(R.drawable.ic_launcher);
     	    img = (ImageView)findViewById(R.id.emoticon);
@@ -69,11 +102,11 @@ public class ListActivity extends Activity {
 	     * Controllando se è possibile la modifica si permetta la stessa o meno
 	     */
         else{
-    	    boolean editable = selectedEntry.canBeUpdated();
+    	    boolean editable = selected.canBeUpdated();
     	    ImageView img = (ImageView) findViewById(R.id.dailyPhoto); 
-    	    img.setImageURI(Uri.parse(selectedEntry.getPhoto().getPath()));
+    	    img.setImageURI(Uri.parse(selected.getPhoto().getPath()));
     	    ImageView mood = (ImageView)findViewById(R.id.emoticon);
-    	    mood.setImageURI(Uri.parse(selectedEntry.getMood().getPathImage()));
+    	    mood.setImageURI(Uri.parse(selected.getMood().getPathImage()));
     	    if(editable){
     	    	 img.setOnTouchListener(new OnTouchListener()
     	         {
@@ -94,19 +127,7 @@ public class ListActivity extends Activity {
     	    		}
     	    	 });
     	    }
-    	    LinearLayout notes = (LinearLayout)findViewById(R.id.notes);
-    	    List<Note> myNotes = selectedEntry.getNotes();
-    	    for(int i = 0;i < myNotes.size();i++){
-    	    	EditText tmp = new EditText(this);
-    	    	tmp.append(myNotes.get(i).toString());
-    	    	tmp.setFocusable(editable);
-    	    	notes.addView(tmp);
-    	    }
         }
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        TextView yourEditText = (TextView)findViewById(R.id.EditText02);
-		imm.showSoftInput(yourEditText, InputMethodManager.SHOW_FORCED);
-		setContentView(R.layout.main);
 	}
 
 

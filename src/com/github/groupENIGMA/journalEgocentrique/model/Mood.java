d0ff6533f603b3f63f6238ce1bf8a29c3717803e
IgorRuz <igor.ruzojcic@gmail.com>
2013-05-27 02:27:36 +0200

    Implemented Mood constructor and methods
    - The constructor is implemented to receive only the Name of the mood
    and associates it to the right path
    - MISSING: the Path string of the mood images

--- src/com/github/groupENIGMA/journalEgocentrique/model/Mood.java
+++ src/com/github/groupENIGMA/journalEgocentrique/model/Mood.java
@@ -1,17 +1,44 @@
 package com.github.groupENIGMA.journalEgocentrique.model;
 
 public class Mood implements MoodInterface {
+	
+	//Mood image paths
+    public static final String HAPPY_IMAGE_PATH = ;
+    public static final String SAD_IMAGE_PATH = ;
+    public static final String ANGRY_IMAGE_PATH = ;
+    public static final String BORED_IMAGE_PATH = ;
+    public static final String APATHETIC_IMAGE_PATH = ;
+    public static final String DEPRESSED_IMAGE_PATH = ;
+	
+	private String Name;
+	private String Path;
+	
+	public Mood(String name){
+		Name = name;
+		if(Name.equals("Happy"))
+			Path = HAPPY_IMAGE_PATH;
+		else if(Name.equals("Sad"))
+			Path = SAD_IMAGE_PATH;
+		else if(Name.equals("Angry"))
+			Path = ANGRY_IMAGE_PATH;
+		else if(Name.equals("Bored"))
+			Path = BORED_IMAGE_PATH;
+		else if(Name.equals("Apathetic"))
+			Path = APATHETIC_IMAGE_PATH;
+		else if(Name.equals("Depressed"))
+			Path = DEPRESSED_IMAGE_PATH;
+	}
 
 	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public String getName() {
+		
+		return Name;
 	}
 
 	@Override
 	public String getPathImage() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return Path;
 	}
 
 }

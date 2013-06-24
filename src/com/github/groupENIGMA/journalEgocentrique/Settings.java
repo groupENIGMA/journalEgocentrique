3e56ba83b218a3cbbf44be696628837f938efcf1
matteovalerio2006@hotmail.it <matteovalerio2006@hotmail.it>
2013-06-24 15:42:04 +0200

    [Settings.java]Added spinners for choosing options
    Now it's possible to choose some options defined in strings.xml. I've not
    yet implemented the listener to the three adapters.

--- src/com/github/groupENIGMA/journalEgocentrique/Settings.java
+++ src/com/github/groupENIGMA/journalEgocentrique/Settings.java
@@ -1,8 +1,9 @@
 package com.github.groupENIGMA.journalEgocentrique;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Settings extends Activity {
 
@@ -10,7 +11,23 @@ public class Settings extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		setAdapters();
 	}
 	
-	// DA FARE LE OPZIONI
+	// Set the adapters for displaying the correct font size and type and the update interval for mood and notes
+	private void setAdapters(){
+		Spinner font = (Spinner)findViewById(R.id.textFont);
+		ArrayAdapter<CharSequence> fontAdapter = ArrayAdapter.createFromResource(this, R.array.textFont, android.R.layout.simple_spinner_item);
+		fontAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		font.setAdapter(fontAdapter);
+		
+		Spinner size = (Spinner)findViewById(R.id.textSize);
+		ArrayAdapter<CharSequence> sizeAdapter = ArrayAdapter.createFromResource(this, R.array.textSize, android.R.layout.simple_spinner_item);
+		size.setAdapter(sizeAdapter);
+		
+		Spinner updateInterval = (Spinner)findViewById(R.id.updateInterval);
+		ArrayAdapter<CharSequence> updateAdapter = ArrayAdapter.createFromResource(this, R.array.updateInterval, android.R.layout.simple_spinner_item);
+		updateInterval.setAdapter(updateAdapter);
+		//TODO-Listener alle liste che permettano di modificare realmente l'aspetto
+	}
 }

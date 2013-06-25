16f6b78bd70a6fe6ab7a5ac0bbfe732bb2443301
IgorRuz <igor.ruzojcic@gmail.com>
2013-06-26 00:55:00 +0200

    Added ImagePinchZoomView and ImageTapZoomView classes.
    - implemented entire ImagePinchZoomView (not tested at all)
    - implemented entire ImageTapZoomView (not tested at all)
    - modified FullScreenPhoto to call the two view (Not sure about that,
    surely to adjust)

--- src/com/github/groupENIGMA/journalEgocentrique/FullScreenPhoto.java
+++ src/com/github/groupENIGMA/journalEgocentrique/FullScreenPhoto.java
@@ -2,6 +2,8 @@ package com.github.groupENIGMA.journalEgocentrique;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -14,8 +16,20 @@ public class FullScreenPhoto extends Activity {
 		setContentView(R.layout.activity_full_screen_photo);
 		Intent intent = getIntent();
 		String path = intent.getStringExtra("Path");
-		ImageView img = (ImageView)findViewById(R.id.photo);
+		ImageView img = (ImageView) findViewById(R.id.photo);
 		img.setImageURI(Uri.parse(path));
-	}
 
+		ImageTapZoomView imageView = new ImageTapZoomView(this, getWindow()
+				.getWindowManager().getDefaultDisplay().getOrientation());
+		this.setContentView(imageView);
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		Bitmap bmp = BitmapFactory.decodeFile(path, options);
+
+		ImagePinchZoomView impz = new ImagePinchZoomView(this);
+		impz.setImageBitmap(bmp);
+		impz.setMaxZoom(4f);
+		setContentView(img);
+	}
 }

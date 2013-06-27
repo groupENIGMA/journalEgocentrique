b992e0ceeb92f18ba2c778d8454912f9b7dbc6e4
matteovalerio2006@hotmail.it <matteovalerio2006@hotmail.it>
2013-06-27 11:46:16 +0200

    [ImageActivity]Fixed a small bug. Now it sets the image

--- src/com/github/groupENIGMA/journalEgocentrique/ImageActivity.java
+++ src/com/github/groupENIGMA/journalEgocentrique/ImageActivity.java
@@ -2,8 +2,12 @@ package com.github.groupENIGMA.journalEgocentrique;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class ImageActivity extends Activity {
@@ -16,6 +20,22 @@ public class ImageActivity extends Activity {
 		String path = intent.getStringExtra(GalleryActivity.EXTRA_MESSAGE);
 		ImageView img = (ImageView)findViewById(R.id.img);
 		img.setImageURI(Uri.parse(path));
+		
+		ImageTapZoomView imageView = new ImageTapZoomView(this, getWindow()
+				.getWindowManager().getDefaultDisplay().getOrientation());
+
+		imageView.setImage(img.getDrawable(), this);
+		this.setContentView(imageView);
+
+/*		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		Bitmap bmp = BitmapFactory.decodeFile(path, options);
+
+		ImagePinchZoomView impz = new ImagePinchZoomView(this);
+		impz.setImageBitmap(((BitmapDrawable)img.getDrawable()).getBitmap());
+		impz.setMaxZoom(4f);
+		((ViewGroup)img.getParent()).removeView(img);
+		this.setContentView(img);*/
 	}
 
 }
